b5f896a95ad1ab5881df48bbf810d8c755cb7103
Le Xuan Lam <lexuanlam30196@gmail.com>
2018-01-12 14:59:43 +0700

    i18n

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,4 +5,14 @@ class ApplicationController < ActionController::Base
   def hello
     render html: "hello, world!"
   end
+
+  protect_from_forgery
+
+  before_action :set_locale
+
+  private
+
+  def set_locale
+    I18n.locale = params[:locale] || I18n.default_locale
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,26 @@
 
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  #root 'static_pages#home'
-  get  'static_pages/home'
-  get  'static_pages/help'
-  get  'static_pages/about'
+  root "static_pages#home"
+  get "static_pages/home"
+  get "static_pages/help"
+  get "static_pages/about"
+  get "static_pages/contact"
+
+  scope "(:locale)", locale: /en|vi/ do
+    root "static_pages#home"
+    get "static_pages/home"
+  end
+
+  scope "(:locale)", locale: /en|vi/ do
+    get "static_pages/help"
+  end
+
+  scope "(:locale)", locale: /en|vi/ do
+    get "static_pages/about"
+  end
+
+  scope "(:locale)", locale: /en|vi/ do
+    get "static_pages/contact"
+  end
 end
